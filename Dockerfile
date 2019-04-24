02349f74c9a0368ea771e165efef963d2e0072ce
Bai Guodong <bgd_sh@hotmail.com>
2019-04-24 23:02:44 +0800

    chore(): for deployment

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM keymetrics/pm2:10-alpine
+RUN apk add tzdata
+RUN apk add dpkg
+RUN apk add python python-dev
+RUN apk add make
+RUN apk add g++
+ENV TZ=Asia/Shanghai
+ENV NODE_ENV=production
+ENV LOG_PATH=/var/logs
+RUN ln -snf /usr/share/zoneinfo/${TZ} /etc/localtime
+RUN echo ${TZ} > /etc/timezone
+RUN npm install yarn -g
+RUN npm install typescript -g
+RUN mkdir -p /usr/share/pardjs-service
+WORKDIR /usr/share/pardjs-service
+COPY ./package.json /usr/share/pardjs-service
+COPY ./yarn.lock /usr/share/pardjs-service
+RUN yarn
+COPY . /usr/share/pardjs-service
+RUN yarn run build
+RUN ls -al
+CMD yarn run start:prod
--- src/main.ts
+++ src/main.ts
@@ -8,16 +8,18 @@ import * as VERSION from '../version.json';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
+  app.setGlobalPrefix('/api');
   const options = new DocumentBuilder()
     .setTitle('Pardjs Form Service')
     .setDescription('The form service of Pardjs')
     .setContactEmail('contact@dozto.com')
     .setVersion(VERSION.api)
+    .setBasePath(process.env.SERVICE_BASE + '/api')
     .addTag('FormService')
+    .setSchemes('http', 'https')
     .build();
   const document = SwaggerModule.createDocument(app, options);
-  SwaggerModule.setup('api-doc', app, document);
+  SwaggerModule.setup('/form-service-api-doc', app, document);
 
   // 使用通用的数据验证 pipe
   app.useGlobalPipes(new ValidationPipe());
