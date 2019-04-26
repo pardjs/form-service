180571abbf8aecc3f95fd41f5c5380b7d6ded750
Bai Guodong <bgd_sh@hotmail.com>
2019-04-26 14:07:50 +0800

    fix(): wrong file extension

--- ormconfig.js
+++ ormconfig.js
@@ -2,9 +2,9 @@ module.exports = {
   type: 'postgres',
   url: process.env.DB_URL || 'postgres://dozto:Passw0rd@localhost:5432/form',
   entities: [
-    process.env.NODE_ENV === 'production'
-      ? 'dist/**/*.entity.ts'
-      : 'src/**/*.entity.ts',
+    process.env.NODE_ENV === 'production' ?
+    'dist/**/*.entity.js' :
+    'src/**/*.entity.ts',
   ],
   synchronize: true,
 };
